68016015eae5462cf47aca80eff0fffa1ea96288
TowardtheStars <hty19980402@qq.com>
2020-03-07 00:37:06 +0800

    植物适应性系统配置

--- src/main/java/com/github/towardthestars/localspecialties/config/Configs.java
+++ src/main/java/com/github/towardthestars/localspecialties/config/Configs.java
@@ -7,7 +7,7 @@ public class Configs
     public static final String CONFIG_DIR = Paths.get("config", "localspecialties").toString();
     public static final MainConfig MAIN = new MainConfig(Paths.get(CONFIG_DIR, "main.toml").toFile());
     public static final SeasonConfig SEASON = new SeasonConfig(Paths.get(CONFIG_DIR, "season.toml").toFile());
-
+    public static final PlantAffinityConfig PLANT_AFFINITY = new PlantAffinityConfig(Paths.get(CONFIG_DIR, "plant_affinity").toFile());
 
     public static void save(){
         MAIN.save();
