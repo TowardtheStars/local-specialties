94a7004001d8cc5c55bfe1634dcf864473dd82ff
TowardtheStars <hty19980402@qq.com>
2020-03-11 16:19:52 +0800

    使用 Registry 实现策略模型

---
+++ src/main/java/com/github/towardthestars/localspecialties/Registries.java
@@ -0,0 +1,17 @@
+package com.github.towardthestars.localspecialties;
+
+import com.github.towardthestars.localspecialties.environment.attribute.EnvAttribute;
+import com.github.towardthestars.localspecialties.plant.attribute.PlantAttribute;
+import com.github.towardthestars.localspecialties.plant.attribute.affinity_model.IAffinityModel;
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import net.minecraft.util.registry.DefaultedRegistry;
+import net.minecraft.util.registry.SimpleRegistry;
+
+public class Registries
+{
+    public static final SimpleRegistry<EnvAttribute> ENV_ATTRIBUTE = new SimpleRegistry<>();
+    public static final SimpleRegistry<PlantAttribute> PLANT_ATTRIBUTE = new SimpleRegistry<>();
+    public static final DefaultedRegistry<Class<? extends IAffinityModel>> AFFINITY_MODEL
+            = new DefaultedRegistry<>(LocalSpecialties.getIdentifier("default0").toString());
+}
